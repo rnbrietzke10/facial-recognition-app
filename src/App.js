05742b0cb8fd2ac929a151c6341f990090ef9443
Rebecca Brietzke <rnbrietzke@outlook.com>
2021-12-17 01:42:08 -0600

    set up signin functionality to send a request to the server and recieve a response of success or error logging in

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
-import Particles from 'react-tsparticles';
-import Clarifai from 'clarifai';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import FaceRecognition from './FaceRecognition/FaceRecognition';
-import Signin from './components/Signin/Signin';
-import Register from './components/Register/Register';
-import './App.css';
+import React, { Component } from "react";
+import Particles from "react-tsparticles";
+import Clarifai from "clarifai";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./FaceRecognition/FaceRecognition";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
+import "./App.css";
 
 const app = new Clarifai.App({
-  apiKey: '4b302c6537874b5f9919e9dba4892ecf',
+  apiKey: "4b302c6537874b5f9919e9dba4892ecf",
 });
 
 const particlesOptions = {
   fpsLimit: 60,
   interactivity: {
-    detectsOn: 'canvas',
+    detectsOn: "canvas",
     events: {
       onClick: {
         enable: true,
-        mode: 'push',
+        mode: "push",
       },
       onHover: {
         enable: true,
-        mode: 'repulse',
+        mode: "repulse",
       },
       resize: true,
     },
@@ -47,10 +47,10 @@ const particlesOptions = {
   },
   particles: {
     color: {
-      value: '#ffffff',
+      value: "#ffffff",
     },
     links: {
-      color: '#ffffff',
+      color: "#ffffff",
       distance: 150,
       enable: true,
       opacity: 0.5,
@@ -60,9 +60,9 @@ const particlesOptions = {
       enable: true,
     },
     move: {
-      direction: 'none',
+      direction: "none",
       enable: true,
-      outMode: 'bounce',
+      outMode: "bounce",
       random: false,
       speed: 2,
       straight: false,
@@ -78,7 +78,7 @@ const particlesOptions = {
       value: 0.5,
     },
     shape: {
-      type: 'circle',
+      type: "circle",
     },
     size: {
       random: true,
@@ -92,10 +92,10 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: '',
-      imageUrl: '',
+      input: "",
+      imageUrl: "",
       box: {},
-      route: 'signin',
+      route: "signin",
       isSignedIn: false,
     };
   }
@@ -106,7 +106,7 @@ class App extends Component {
     const clarifaiFace =
       data.outputs[0].data.regions[0].region_info.bounding_box;
 
-    const image = document.getElementById('inputImage');
+    const image = document.getElementById("inputImage");
     const width = Number(image.width);
     const height = Number(image.height);
     return {
@@ -139,9 +139,9 @@ class App extends Component {
       });
   };
   onRouteChange = (route) => {
-    if (route === 'signout') {
+    if (route === "signout") {
       this.setState({ isSignedIn: false });
-    } else if (route === 'home') {
+    } else if (route === "home") {
       this.setState({ isSignedIn: true });
     }
     this.setState({ route: route });
@@ -156,7 +156,7 @@ class App extends Component {
           isSignedIn={isSignedIn}
           onRouteChange={this.onRouteChange}
         />
-        {route === 'home' ? (
+        {route === "home" ? (
           <div>
             <Logo />
             <Rank />
@@ -166,7 +166,7 @@ class App extends Component {
             />
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
-        ) : route === 'signin' ? (
+        ) : route === "signin" ? (
           <Signin onRouteChange={this.onRouteChange} />
         ) : (
           <Register onRouteChange={this.onRouteChange} />
